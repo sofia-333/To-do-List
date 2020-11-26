475c03ead97cf3cca9584186e1f6357f22d22948
sofia-333 <sofio.muradovi@gmail.com>
2020-11-26 19:22:15 +0400

    added document formatting

--- tl.js
+++ tl.js
@@ -24,28 +24,28 @@ inputTodo.value = "";
 var ul = document.getElementsByTagName('ul')[0];
 
 var addButton = document.getElementById('add');
-addButton.addEventListener('click',addToList);
+addButton.addEventListener('click', addToList);
 
-function addToList(e){
+function addToList(e) {
     //create new checkbox element with value from input
     var newTodo = document.createElement('input');
     newTodo.type = 'checkbox';
-    
+
     //create label and write newTodo there
     var label = document.createElement('label');
     label.innerText = inputTodo.value;
     // console.log(newTodo);
-    
+
     //create new delete button
     var newDelBtn = document.createElement('input');
-    newDelBtn.type = 'button'; 
-    newDelBtn.classList.add('delete'); 
-    newDelBtn.value='x'; 
-    
+    newDelBtn.type = 'button';
+    newDelBtn.classList.add('delete');
+    newDelBtn.value = 'x';
+
     //create new list
     var newlist = document.createElement('li');
     newlist.classList.add('li');
-    
+
     //place new del button, checkbox and label in the new list
     newlist.appendChild(newDelBtn);
     newlist.appendChild(newTodo);
@@ -56,13 +56,13 @@ function addToList(e){
     //cler the input after adding a list
     inputTodo.value = "";
     //DELETE button erases the list
-    newDelBtn.addEventListener('click', function(e){
+    newDelBtn.addEventListener('click', function (e) {
         let parentList = e.target.parentElement;
         parentList = ul.removeChild(parentList);
     });
-    
-    newTodo.addEventListener('change', function(e){
-        if(this.checked){
+
+    newTodo.addEventListener('change', function (e) {
+        if (this.checked) {
             let parentList = e.target.parentElement;
             //get label of checked todo
             let textOfchecked = parentList.getElementsByTagName('label')[0];
@@ -71,7 +71,7 @@ function addToList(e){
             ul.removeChild(e.target.parentElement);
             ul.appendChild(parentList);
 
-        }else{
+        } else {
             let parentList = e.target.parentElement;
             let textOfchecked = e.target.parentElement.getElementsByTagName('label')[0];
             textOfchecked.style.textDecoration = 'none';
